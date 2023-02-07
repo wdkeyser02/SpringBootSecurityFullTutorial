0fa18915670954e432324b9d62eb14e156db4476
willydekeyser <ma074172@skynet.be>
2023-02-07 19:59:48 +0100

    Setup project.

---
+++ SpringBootSecurityFullTutorial/src/test/java/willydekeyser/SpringBootSecurityFullTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityFullTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
