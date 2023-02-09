f562c8dbe96471758713e5cd4988dbfdd13a6dc2
willydekeyser <ma074172@skynet.be>
2023-02-09 16:12:54 +0100

    Update

--- SpringBootSecurityFullTutorial/src/main/java/willydekeyser/controller/HomeController.java
+++ SpringBootSecurityFullTutorial/src/main/java/willydekeyser/controller/HomeController.java
@@ -1,5 +1,7 @@
 package willydekeyser.controller;
 
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,26 +19,56 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "<h1>Welcome home!</h1>";
+		return """ 
+				<h1>Welcome home!</h1>
+				<a href="/user">User</a><br/>
+				<a href="/admin">Admin</a><br/>
+				<a href="/developer">Developer</a>
+				""";
 	}
 	
 	@GetMapping("/user")
 	public String user(Authentication authentication) {
 		UserDto user = userService.getLoginUser();
-		return "<h1>Welcome User!</h1><h2>" + authentication.getName() + "</h2><h3>" + user + "</h3>";
+		return """ 
+				<h1>Welcome User!</h1>
+				<h2>%s</h2>
+				<h3>%s</h3>
+				""".formatted(authentication.getName(), user);
 	}
 	
 	@GetMapping("/admin")
 	public String admin(Authentication authentication) {
 		MySecurityUser myUser = (MySecurityUser) authentication.getPrincipal();
-		return "<h1>Welcome Admin!</h1><h2>" + authentication.getName() + "</h2><p>MyUser:<br>User name: " + myUser.getUsername()  
-		+ "<br>Full name: " + myUser.getFullname() + "<br>E-Mail: " + myUser.getEmailaddress() + "<br>Birthday: " + myUser.getBirthdate()
-		+ "<br>Authorities: " + myUser.getAuthorities();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return """ 
+				<h1>Welcome Admin!</h1>
+				<h2>%s</h2>
+				<p>MyUser:<br/>
+				User name: %s<br/>
+				Full name: %s<br/>
+				E-Mail: %s<br/>
+				Birthday: %s<br/>
+				Authorities: %s
+				</p>
+				""".formatted(
+						authentication.getName(), 
+						myUser.getUsername(),
+						myUser.getFullname(),
+						myUser.getEmailaddress(),
+						myUser.getBirthdate().format(dateTimeFormatter),
+						myUser.getAuthorities()
+						);
+		
 	}
 	
 	@GetMapping("/developer")
 	public String developer(Authentication authentication) {
 		UserDto user = userService.getLoginUser();
-		return "<h1>Welcome User!</h1><h2>" + authentication.getName() + "</h2><h3>" + user + "</h3>";
+		return """ 
+				<h1>Welcome Developer!</h1>
+				<h2>%s</h2>
+				<h3>%s</h3>
+				""".formatted(authentication.getName(), user);
 	}
 }
